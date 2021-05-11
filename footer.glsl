b418eff1a2fb2c80b9074b7fd25abd3fe97e9c68
shashi278 <shashiranjankv@gmail.com>
2021-05-11 23:27:56 +0530

    Moved files

--- footer.glsl
+++
@@ -1,8 +0,0 @@
-void main( void ) {
-    vec2  uv = gl_FragCoord.xy / resolution.xy;
-    if(direction == 1.0){progress = 1.0 - t;}
-    else{progress = t;}
-
-    gl_FragColor = transition(uv);
-}
-
